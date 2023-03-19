4a6f43a5b3f1bb309c4a059abbe18861a0306ea3
yuniko02 <113278118+yuniko02@users.noreply.github.com>
2023-03-19 11:43:05 +0900

    change

--- js/main.js
+++ js/main.js
@@ -1,36 +1,47 @@
 $(function($) {
-    $('.slider').slick({
+    $(".slider").slick({
         arrows: true,
         autoplay: true,
         adaptiveHeight: true,
         dots: false,
         centerMode: true,
-        centerPadding: '40px',
+        centerPadding: '20px',
         slidesToShow: 3,
-      });
 
-      $('.slider2').slick({
+        responsive:[
+            {
+                breakpoint:501
+                settings:{
+                    arrows: false,
+                    dots: true,
+                    slidersToShow: 1,
+                },
+            },
+        ],
+    });
+
+    $('.slider2').slick({
         arrows: true,
         autoplay: true,
         adaptiveHeight: true,
         dots: false,
         centerMode: true,
-        centerPadding: '40px',
+        centerPadding: '20px',
         slidesToShow: 3,
         rtl: true,
-      });
+    });
     
-      $("#back-top").hide();
-      $(window).scroll(function () {
-          if ($(window).scrollTop() > 500) {
-              $("#back-top").fadeIn(1000);
-          } else {
-              $("#back-top").fadeOut();
-          }
-      });
-      $("#back-top").click(function () {
-          $("body,html").animate({scrollTop: 0}, 500);
-          return false;
-      });
+    $("#back-top").hide();
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 500) {
+            $("#back-top").fadeIn(1000);
+        } else {
+            $("#back-top").fadeOut();
+        }
+    });
+    $("#back-top").click(function () {
+        $("body,html").animate({scrollTop: 0}, 500);
+        return false;
+    });
 
 });
